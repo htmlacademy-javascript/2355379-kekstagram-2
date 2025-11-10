998418a4a5a4064ce2a725210b3b699e53a50656
Игорь Алексеевич <webpr@list.ru>
2025-11-11 02:23:58 +1000

    Убирает ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,6 @@
 // Функция для проверки длины строки.
-const str = 'Лёша на полке клопа нашёл ';
-const len = 27;
 const checkLength = (string, maxLength) => {
-	return (string.length <= maxLength) ? true : false;
+	return string.length <= maxLength;
 };
 
 // Функция для проверки, является ли строка палиндромом
@@ -12,21 +10,18 @@ const checkPalindrome = (string) => {
 	for(let i = string.length - 1; i >= 0; --i){
 		strReverse += string[i];
 	};
-	return string === strReverse ? true : false;
-
+	return string === strReverse;
 };
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-const strg = 'ECMAScript 2022';
-let num = '';
+
 const getNumbers = (string) => {
+  let num = '';
 	for(let i = 0; i <= string.length - 1; ++i) {
 		if((string[i] === '0') || (Number(string[i]))) {
 			num += string[i];
 		}
 	}
-	Number(num);
 	num = parseFloat(num);
-	console.log(num);
 	return num;
 };
