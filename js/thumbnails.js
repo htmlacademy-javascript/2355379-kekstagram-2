c158aa1859b5d9de6632fbe42452600d1b3b828d
Garry <webpr@list>
2025-12-10 08:09:02 +0300

    откуда photo

---
+++ js/thumbnails.js
@@ -0,0 +1,9 @@
+const template = document.querySelector('#picture').textContent.querySelector('.picture');
+
+const image = template.querySelector('.picture__img');
+image.src = photo.url;
+image.alt = photo.description;
+
+
+
+
